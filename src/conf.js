ff3b3d02001f94313189569c5c5f8ab6a0e4d2f6
yunhe.ww <yunhe.ww@alibaba-inc.com>
2020-06-24 00:36:57 +0800

    disable dd runtime check.

--- src/conf.js
+++ src/conf.js
@@ -1,5 +1,5 @@
 export default {
-  disableDingTalk: false,
+  disableDingTalk: true,
   amapKey: '753ea77c5a4638f9ce4ea33de8400335',
   corpId: 'ding00171aa88c902cffacaaa37764f94726',
   apiBaseUrl: 'http://api-rextkshuiliweb791128.vaiwan.com/api',
